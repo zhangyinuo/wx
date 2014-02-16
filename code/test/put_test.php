8bcc1872f13fad2fd3a968924381775477ce1431
jingchun.zhang <danezhang77@gmail.com>
2014-02-16 17:58:24 +0800

    msg

--- code/test/put_test.php
+++ code/test/put_test.php
@@ -5,16 +5,13 @@ $ROOTDIR=dirname(__FILE__)."/../";
 require_once($ROOTDIR."log/log.php");
 require_once($ROOTDIR."queue/queue.php");
 
-$subq = "";
-if (init_q($subq, $sub_queue_file, "p") === false)
-{
-	runlog(__FILE__."_".__LINE__.":"."ERR init_ftok $sub_queue_file !");
-	exit;
-}
+$pexe = $ROOTDIR."/getlist/phantomjs";
+$sendjs = $ROOTDIR."/test/weixin_mass.js";
 
-$msg = "self_test&&oMj31t2-QJyDQ8U_Eix5btP3LwYo&&1391347447&&faint";
-
-msg_send($subq, 1, $msg);
+runlog("$pexe $sendjs $username $passwd $fid $msg");
+$fp = popen("$pexe $sendjs $username $passwd $fid '$msg'", "r");
+if ($fp)
+	pclose($fp);
 
 ?>
 
