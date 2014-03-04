0623c33c8dbce773bd9adc794eed6c3b4b08404f
danezhang <270094581@qq.com>
2014-03-04 16:40:58 +0800

    msg

--- code_edu/conf/code.php
+++ code_edu/conf/code.php
@@ -13,9 +13,20 @@ function wx_reg_parse($src)
 	return $ret;
 };
 
-function wx_test_exe($srcarr)
+function wx_test_exe($msg, $dblink)
 {
-	runlog(__FILE__."_".__LINE__);
+	$result = mysql_query("select wx_username from t_wx_info ", $dblink);
+	if ($result === false)
+	{
+		runlog("query wx_username from t_wx_info is nul:");
+		return false;
+	}
+	while($row=mysql_fetch_array($result)) 
+	{
+		runlog(__FILE__."_".__LINE__.":".$row[0]);
+	}
+	mysql_free_result($result);
+	runlog(__FILE__."_".__LINE__.":".$msg);
 };
 
 function wx_test_parse($src)
