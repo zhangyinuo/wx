1e29e0ecce1813487297265c6524944fdca7b190
张敬春 <zhangjingchun@zhangjingchundeMacBook-Pro.local>
2014-03-08 20:42:27 +0800

    msg

--- code_touzi/db/initdb.sql
+++ code_touzi/db/initdb.sql
@@ -6,7 +6,6 @@ CREATE TABLE t_wx_info (
 	`id` int(11) NOT NULL AUTO_INCREMENT, 
 	`fakeid` varchar(32) not null,
 	`wx_username` varchar(32) not null,
-	`wx_flag` varchar(1) not null default '0',
 	`modtime` varchar(16) not null,
 	`lasttime` int ,
 	`laststep` varchar(16) default null,
@@ -14,27 +13,4 @@ CREATE TABLE t_wx_info (
 	unique key(fakeid),
 	unique key(wx_username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE t_teacher_info (
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`name` varchar(32) not null,
-	`status` int not null default '1',
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE t_course_info (
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`name` varchar(32) not null,
-	`status` int not null default '1',
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE t_class_info (
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`name` varchar(32) not null,
-	`teacherid` int not null,
-	`status` int not null default '1',
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
