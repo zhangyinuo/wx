ed158c6092db4dc546ba7577a98c86df44156ed4
danezhang <270094581@qq.com>
2014-03-08 20:38:20 +0800

    msg

--- code_touzi/db/initdb.sql
+++ code_touzi/db/initdb.sql
@@ -1,16 +1,15 @@
-create database wx_edu_schoolname;
+create database wx_touzi;
 
-use wx_edu_schoolname;
+use wx_touzi;
 
 CREATE TABLE t_wx_info (
 	`id` int(11) NOT NULL AUTO_INCREMENT, 
 	`fakeid` varchar(32) not null,
 	`wx_username` varchar(32) not null,
 	`wx_flag` varchar(1) not null default '0',
-	`bind_sid` varchar(64),
-	`bind_tid` varchar(64),
-	`bind_cid` varchar(64),
 	`modtime` varchar(16) not null,
+	`lasttime` int ,
+	`laststep` varchar(16) default null,
 	PRIMARY KEY (`id`),
 	unique key(fakeid),
 	unique key(wx_username)
