ecd07bdca9bbe5fba3c2782ae2a982721b1abb9d
danezhang <270094581@qq.com>
2014-03-27 20:52:06 +0800

    msg

---
+++ phpmini/touzi/money.php
@@ -0,0 +1,28 @@
+ <table id="content">
+  <tr>
+   <td id="sidebar-left"><div class="block block-user" id="block-user-1">
+ <div class="content">
+<ul class="menu">
+<li class="leaf"><a href="account.php" class="active">我的帐户</a></li>
+<li class="leaf"><a href="logout.php">注销登录</a></li>
+
+</ul>
+</div>
+</div>
+</td>
+   <td id="main">
+
+<!-- begin content -->
+<div class="profile"><h2 class="title">金融资讯管理, 按顺序显示</h2>
+<div class="leaf"><a href="do_www.php?type=2&id=1" >条目1</a></div>
+<div class="leaf"><a href="do_www.php?type=2&id=2" >条目2</a></div>
+<div class="leaf"><a href="do_www.php?type=2&id=3" >条目3</a></div>
+<div class="leaf"><a href="do_www.php?type=2&id=4" >条目4</a></div>
+
+<!-- end content -->
+   </td>
+  </tr>
+ </table>
+
+ </body>
+</html>
